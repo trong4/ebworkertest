dd9bfcc663667e8ad467cdf6f3f7a9cd55ded3d3
trong <devgruxcon@gmail.com>
2016-12-28 12:27:43 +0000

    receiving job

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'hello worker' });
+    console.log("receiving job on root path /")
+    console.log("query", req.query)
+    console.log("body", req.body)
+    console.log("name", req.body.name)
+    console.log("title", req.body.title)
+    res.send({ok:true})
+    // res.render('index', { title: 'hello worker' });
 });
 
 module.exports = router;
