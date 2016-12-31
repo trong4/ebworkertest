584a5ac5991ecb914d5cfb566b0fece87db54470
trong <devgruxcon@gmail.com>
2016-12-31 06:58:19 +0000

    test dynamodb config

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
+var AWS = require('aws-sdk');
 var express = require('express');
 var router = express.Router();
+
+var db = new AWS.DynamoDB();
+var users_table =  process.env.USERS_TABLE;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,6 +14,18 @@ router.post('/', function(req, res, next) {
     console.log(new Date())
     console.log(req.body)
     res.send({ok:true})
+
+    db.putItem({
+        'TableName': users_table,
+        'Item': {
+            'username': {'S': req.body.username},
+            "created": {"S": new Date().toISOString()},
+        },
+        'Expected': { username: { Exists: false } }
+    }, function(er, data){
+        if (er) return console.log(new Date(), 'DB Error:', er)
+        console.log(new Date(), "DB data", data)
+    });
 });
 
 module.exports = router;
